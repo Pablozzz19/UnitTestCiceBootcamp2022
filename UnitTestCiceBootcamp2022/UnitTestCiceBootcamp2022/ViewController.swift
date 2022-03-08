c03a3d1410f02af04a7c66a6fecffcea985ae078
Pablozzz19 <72037798+Pablozzz19@users.noreply.github.com>
2022-03-08 20:14:11 +0100

    [FEATURE] - Init App using Storyboard

---
+++ UnitTestCiceBootcamp2022/UnitTestCiceBootcamp2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnitTestCiceBootcamp2022
+//
+//  Created by cice on 08/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
